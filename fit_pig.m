e3d07b64c960742fafde5eb8587f9b1dbb39630c
Jonathan Tompson <jonathantompson@gmail.com>
2014-07-29 22:29:32 -0400

    ICP works (but strangely the BFGS method fails)

--- fit_pig.m
+++ fit_pig.m
@@ -1,33 +1,47 @@
 clearvars; clc; close all;
 
+display('Loading the models from file');
 pig = obj_read('PigData\results\id110\id_110_merged.obj');
-pc2 = read_off('PigData\results\id110\part_1.off')';
+pc2 = read_off('PigData\results\id110\part_1.off');
 
-% icp_data = struct;
-% icp_data.num_iterations = 29;
-% icp_data.pc1 = bunny_data.vert';
-% icp_data.npc1 = bunny_data.norm';  % Optional parameter
-% icp_data.pc2 = pc2';
-% icp_data.npc2 = npc2';  % Optional parameter
-% icp_data.m_pc2_initial = eye(4);
-% icp_data.min_distance_sq = 1e-6;  % Optional parameter
-% icp_data.max_distance_sq = 1e+3;  % Optional parameter
-% icp_data.cos_normal_threshold = 0.9;  % Optional parameter
-% icp_data.method = 1;  % Optional parameter
-% 
-% M_PC2_icp = icp(icp_data);
-% 
-% % Affine Transform position
-% pc2_icp = (M_PC2_icp(1:3,1:3) * pc2' + repmat(M_PC2_icp(1:3,4), 1, size(bunny_data.vert,1)))';
+% center the vertices at zero
+pig.Position = pig.Position - repmat(mean(pig.Position, 2), 1, size(pig.Position,2));
+pc2 = pc2 - repmat(mean(pc2, 2), 1, size(pc2,2));
 
+% Now apply a global scale so that the coordinates are between 0 and 1
+scale = max(pig.Position(:)) - min(pig.Position(:));
+pig.Position = pig.Position / scale;
+pc2 = pc2 / scale;
+
+% Start pc2 in a reasonable position
+M_init = euler2RotMat(0, pi/4, 0);
+M_init = rightMultScale(M_init, 1, 1, 1);
+M_init = leftMultTranslation(M_init, 0.2, 0, 0);
+pc2 = (M_init(1:3,1:3) * pc2 + repmat(M_init(1:3,4), 1, size(pc2,2)));
+
+icp_data = struct;
+icp_data.num_iterations = 100;
+icp_data.pc1 = pig.Position;
+% icp_data.npc1 = pig.Normal;  % Optional parameter
+icp_data.pc2 = pc2;
+% icp_data.npc2 = npc2;  % Optional parameter
+icp_data.m_pc2_initial = eye(4);
+icp_data.min_distance_sq = 1e-6;  % Optional parameter
+icp_data.max_distance_sq = 1e+6;  % Optional parameter
+icp_data.cos_normal_threshold = 0.9;  % Optional parameter
+icp_data.method = 3;  % Optional parameter
+ 
+display('Running ICP');
+M_PC2_icp = icp(icp_data);
+
+% Affine Transform position
+pc2_icp = (M_PC2_icp(1:3,1:3) * pc2 + repmat(M_PC2_icp(1:3,4), 1, size(pc2,2)));
+
+display('Displaying the results');
 figure;
 set(gcf, 'Position', [200 200 1200 1200]);
-scatter3(pc2(:,1), pc2(:,2), pc2(:,3),'b.'); hold on;
+scatter3(pc2(1,:), pc2(2,:), pc2(3,:),'b.'); hold on;
+% scatter3(pig.Position(1,:), pig.Position(2,:), pig.Position(3,:),'k.');
 trimesh(pig.Tri'+1, pig.Position(1,:), pig.Position(2,:), pig.Position(3,:));
+scatter3(pc2_icp(1,:), pc2_icp(2,:), pc2_icp(3,:),'k.');
 set(gcf,'renderer','opengl'); axis vis3d; axis equal;
-
-% mean_err_before = mean(sum((pc2 - bunny_data.vert).^2, 2));
-% mean_err_after = mean(sum((pc2_icp - bunny_data.vert).^2, 2));
-
-% display(['Mean error before ICP: ', num2str(mean_err_before)]);
-% display(['Mean error after ICP: ', num2str(mean_err_after)]);
